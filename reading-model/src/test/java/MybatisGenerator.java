a0928b867892048fa341a546dc2d166061e2babc
liuguangfu <13814516352@139.com>
2018-12-05 19:36:16 +0800

    添加收藏功能

--- reading-model/src/test/java/MybatisGenerator.java
+++ reading-model/src/test/java/MybatisGenerator.java
@@ -21,7 +21,7 @@ import java.util.Map;
  */
 public class MybatisGenerator {
 
-    private static String[] tables = new String[]{"album","audio","video"};
+    private static String[] tables = new String[]{"information"};
     private static String BASE_PATH_ROOT = "/D:/program/reading";
     private static String AUTHOR = "liuguangfu";
 
