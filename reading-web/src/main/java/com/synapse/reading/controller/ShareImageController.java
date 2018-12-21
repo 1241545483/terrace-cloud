f67c66be9b312115ce3b298e6a9c3249b75d0b19
liuguangfu <13814516352@139.com>
2018-12-21 20:55:37 +0800

    修改分享图片

--- reading-web/src/main/java/com/synapse/reading/controller/ShareImageController.java
+++ reading-web/src/main/java/com/synapse/reading/controller/ShareImageController.java
@@ -197,12 +197,12 @@ public class ShareImageController extends BaseController {
             @ApiResponse(code = 500, response = String.class, message = "服务器错误")
     })
     @RequestMapping(value = "/v1/shareImage/getShareUrl/{recId}", method = RequestMethod.GET)
-    public ResponseEntity getShareUrl(@PathVariable("recId") String recId,String wxNickName) {
+    public ResponseEntity getShareUrl(@PathVariable("recId") String recId) {
         try {
             User user = UserContext.getUser();
             String shareType = "audio";
 
-            String url = shareImageService.getShareUrl(recId, user,shareType,wxNickName);
+            String url = shareImageService.getShareUrl(recId, user,shareType);
             return ResponseEntity.ok(url);
         } catch (BusinessException e) {
             logger.error("get ShareImage Error!", e);
