9a68d0bc991f3951e6e2704d7cd57d72d9628a10
liuguangfu <13814516352@139.com>
2019-04-08 17:12:39 +0800

    阅读支付接口

--- reading-web/src/main/java/com/synapse/reading/controller/WxPayController.java
+++ reading-web/src/main/java/com/synapse/reading/controller/WxPayController.java
@@ -73,8 +73,8 @@ public class WxPayController {
             pay.setSecurityKey(secret);
             pay.setTradeName("交易订单名称");
             pay.setPartnerSign("商户验签");
-            pay.setTradeType("JSAPI");
-            pay.setPayType("2");
+            pay.setTradeType("2");
+            pay.setPayType("3");
             pay.setPayerAccId(8888l);
 //            payInfo.setShowUrl("服务URL");
             pay.setDeviceType("JSAPI");
