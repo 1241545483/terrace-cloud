f809207f81b8035b14afa2be8e5de9b225e1962f
liuguangfu <13814516352@139.com>
2019-04-18 19:23:08 +0800

    新增批量导入老师学生等管理员接口接口

--- reading-service/src/main/java/com/synapse/reading/remote/UserService.java
+++ reading-service/src/main/java/com/synapse/reading/remote/UserService.java
@@ -31,8 +31,8 @@ public interface UserService {
     boolean userNameIsExist(String loginName) ;
 
     @RequestMapping(value = "/user/v1/shiluregist", method = RequestMethod.GET
-            , consumes = "application/json;charset=UTF-8")
-    BizTrans shiluregist(Map<String,Object> params) ;
+            , produces = "application/json;charset=UTF-8", consumes = "application/json;charset=UTF-8")
+    BizTrans shiluregist(@RequestParam Map<String,Object> params) ;
 
     @RequestMapping(value = "/user/v1/user_id", method = RequestMethod.GET
             , produces = "application/json;charset=UTF-8", consumes = "application/json;charset=UTF-8")
