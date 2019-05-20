ca452fa340db423acd98c12af373bb0643be23ef
liuguangfu <13814516352@139.com>
2019-05-20 18:11:04 +0800

    修改user登录判断

--- reading-service/src/main/java/com/synapse/reading/remote/UserService.java
+++ reading-service/src/main/java/com/synapse/reading/remote/UserService.java
@@ -75,18 +75,22 @@ public interface UserService {
     produces = "application/json;charset=UTF-8", consumes = "application/json;charset=UTF-8")
     public Integer update(@RequestBody OrgModel orgModel);
 
-    @RequestMapping(value ="/org/v1/search",method = RequestMethod.GET)
+    @RequestMapping(value ="/org/v1/search",method = RequestMethod.GET
+    ,produces = "application/json;charset=UTF-8", consumes = "application/json;charset=UTF-8")
     public List<OrgModel> searchGroups(@RequestParam("name")String name,@RequestParam("parentOrgId") String parentOrgId) ;
 
 
-    @RequestMapping(value="/schoolmanager", method = RequestMethod.GET)
+    @RequestMapping(value="/schoolmanager", method = RequestMethod.GET
+     ,produces = "application/json;charset=UTF-8", consumes = "application/json;charset=UTF-8")
     public Map<String, Object> listSchoolManager(PageInfo page,@RequestParam("orgId")String orgId);
 
-    @RequestMapping(value="/modifyuserinfo/{userId}", method = RequestMethod.PUT)
+    @RequestMapping(value="/modifyuserinfo/{userId}", method = RequestMethod.PUT
+     ,produces = "application/json;charset=UTF-8", consumes = "application/json;charset=UTF-8")
     public int modifyUserInfo(@RequestParam("loginname")String loginname, @RequestParam("passwd")String passwd, @PathVariable("userId") String userId);
 
 
-    @RequestMapping(value = "/mini/bind",method = RequestMethod.POST)
+    @RequestMapping(value = "/mini/bind",method = RequestMethod.POST
+     ,produces = "application/json;charset=UTF-8", consumes = "application/json;charset=UTF-8")
     public Result miniBind(@RequestBody Map<String,String> param);
 }
 
